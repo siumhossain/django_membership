8999b68a5ac4d2806d48daf977ea3310abd0ee47
siumhossain <sium.hossain@yahoo.com>
2020-09-13 12:01:29 +0600

    another1

--- db/settings.py
+++ db/settings.py
@@ -84,6 +84,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
